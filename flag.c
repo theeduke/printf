e9882fce7b08316e29ea9870ccb6d2cf89f634d1
theeduke <oscarian528@gmail.com>
2022-08-09 15:13:24 -0700

    flag.c

--- flag.c
+++ flag.c
@@ -4,7 +4,7 @@
  * get_flag - turns on flags if _printf finds.
  * a flag modifier in the format string
  * @s: character that holds the flag specifier
- * @f: pointer to the struct flags in which we turn the flags on *
+ * @f: pointer to the struct flags in which we turn the flags on 
  * Return: 1 if a flag has been turned on, 0 otherwise
  */
 
