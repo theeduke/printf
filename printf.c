026c1db1bf2b49403a23478edf04e788b5f97ccc
theeduke <oscarian528@gmail.com>
2022-08-09 09:52:58 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -1,79 +1,22 @@
 #include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
 /**
- * _printf - function to print output
- * @format: pointer to argument
- * Return: count
+ * _printf - prints out the  output according to a format.
+ * @format: format string containing the characters
+ * Description: this function will call the get_print()
+ * determine which printing function to call depending on
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	int (_pfunc)(va_list, flags_t _);
-	const char *i;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
 	va_list arguments;
 	flags_t flags = {0, 0, 0};
 
-<<<<<<< HEAD
-	int (_pfunc)(va_list, flags_t _);
-
-	const char *i;
-
-	va_list arguments;
-
-	flags_t flags = {0, 0, 0};
-
-	register int count = 0;
-
-	va_start(arguments, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
-
-		return (-1);
-
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-
-		return (-1);
-
-	for (i = format; *i; i++)
-
-    {
-
-	if (*i == '%')
-
-        {
-
-		i++;
-
-		if (*i == '%')
-
-            {
-
-		count += _putchar('%');
-
-		continue;
-
-            }
-
-	while (get_flag(*i, &flags))
-
-		i++;
-
-	pfunc = get_print(*i);
-
-		count += (pfunc)
-
-	? pfunc(arguments, &flags)
-
-		: _printf("%%%c", *i);
-
-        } else
-
-		count += _putchar(*i);
-
-    }
-
-	_putchar(-1);
-    va_end(arguments);
-    
-    return (count);
 	register int count = 0;
 
 	va_start(arguments, format);
@@ -81,24 +24,24 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (i = format; *i; i++)
+	for (p = format; *p; p++)
 	{
-		if (*i == '%')
+		if (*p == '%')
 		{
-			i++;
-			if (*i == '%')
+			p++;
+			if (*p == '%')
 			{
 				count += _putchar('%');
 				continue;
 			}
-			while (get_flag(*i, &flags))
-				i++;
-			pfunc = get_print(*i);
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
 			count += (pfunc)
 				? pfunc(arguments, &flags)
-				: _printf("%%%c", *i);
+				: _printf("%%%c", *p);
 		} else
-			count += _putchar(*i);
+			count += _putchar(*p);
 	}
 	_putchar(-1);
 	va_end(arguments);
